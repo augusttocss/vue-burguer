657e4379ce732cbcc3913b956ab2f87b29550704
Augusto Cezar <augusto.santos@grancursosonline.com.br>
2023-11-28 19:04:16 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <Navbar :logo="logo_src" :alt="app_name"/>
+  <router-view />
+  <Footer />
+</template>
+
+<script>
+  import Navbar from './components/NavBar.vue';
+  import Footer from './components/Footer.vue';
+
+  export default {
+    components:{
+      Navbar,
+      Footer
+    },
+    data(){
+      return{
+        logo_src:"/img/logo.png",
+        app_name:" Projeto Burger",
+      }
+    }
+  }
+</script>
+
+<style>
+ *{
+  font-family: Arial, Helvetica, sans-serif;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+ }
+
+.main-container{
+  margin: 50px;
+  min-height: 350px;
+}
+
+h1{
+  text-align: center;
+  font-size: 40px;
+  margin-bottom: 30px;
+  color: #222;
+}
+</style>
